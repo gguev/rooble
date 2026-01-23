5a1f5b10c8680083596ac80dc0125455106f0911
Guillermo Guevara <guillermo1699@hotmail.com>
2026-01-22 23:27:09 -0500

    style: format Footer component and update email

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,18 +1,32 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 export function Footer() {
   return (
     <footer className="flex h-24 w-full items-center justify-evenly">
       <p> Copyright &copy; 2022. Guillermo Guevara</p>
-      <div className='flex'>
-        <a className="flex text-blue-400 hover:text-blue-600 hover:underline"><Link href={'mailto: guillermo1699@hotmail.com'}>Contact</Link></a>
-        <div className="mx-3">&middot;</div> 
-        <a className="flex text-blue-400 hover:text-blue-600 hover:underline"><Link href={'/about'}>About</Link></a>
-        <div className="mx-3">&middot;</div> 
-        <a className="flex text-blue-400 hover:text-blue-600 hover:underline"><Link href={'/features'}>Features</Link></a>
-                
+      <div className="flex">
+        <Link
+          href={'mailto: gguev_@outlook.com'}
+          className="flex text-blue-400 hover:text-blue-600 hover:underline"
+        >
+          Contact
+        </Link>
+        <div className="mx-3">&middot;</div>
+        <Link
+          href={'/about'}
+          className="flex text-blue-400 hover:text-blue-600 hover:underline"
+        >
+          About
+        </Link>
+        <div className="mx-3">&middot;</div>
+        <Link
+          href={'/features'}
+          className="flex text-blue-400 hover:text-blue-600 hover:underline"
+        >
+          Features
+        </Link>
       </div>
       <p>Watch videos without the clutter</p>
-  </footer>
+    </footer>
   )
 }
