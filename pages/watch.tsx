eb79bf22ee29b50bdc0323f607bb381490b922da
Guillermo Guevara <guillermo1699@hotmail.com>
2026-01-22 23:27:34 -0500

    refactor: update watch page with dynamic Plyr import and simplified props

--- pages/watch.tsx
+++ pages/watch.tsx
@@ -1,33 +1,52 @@
+import { get } from 'http'
+import dynamic from 'next/dynamic'
 import Head from 'next/head'
+import 'plyr-react/plyr.css'
 import Header from '../components/Header/Header'
 import getVideo from '../lib/getVideo'
-import Plyr from "plyr-react"
-import "plyr-react/plyr.css"
 
-const WatchPage = (props: { video: any }) => {
-  const { video } = props
+const Plyr = dynamic(() => import('plyr-react').then((mod) => mod.default), {
+  ssr: false,
+})
+
+const WatchPage = (props: { videoID: string; title: string }) => {
+  const { videoID, title } = props
 
   return (
     <div>
       <Head>
-        <title>{video.title} - Goomba</title>
+        <title>{title} - Rooble</title>
 
         <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>  
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
       </Head>
 
       <Header />
 
       <div className="px-52 mt-2 mb-3">
-        <div className='rounded-lg overflow-hidden'>
-          <Plyr 
+        <div className="rounded-lg overflow-hidden">
+          <Plyr
             source={{
-              type: "video",
-              sources: [{ src: video.id, provider: 'youtube' }],
+              type: 'video',
+              sources: [{ src: videoID, provider: 'youtube' }],
             }}
-          />          
+          />
         </div>
       </div>
     </div>
@@ -37,12 +56,13 @@ const WatchPage = (props: { video: any }) => {
 export async function getServerSideProps(ctx: { query: { v: any } }) {
   const { v: videoID } = ctx.query
 
-  const res = await getVideo(videoID)
+  const vid = await getVideo(videoID)
 
   return {
     props: {
-      video: res
-    }
+      videoID,
+      title: vid.basic_info.title,
+    },
   }
 }
 
